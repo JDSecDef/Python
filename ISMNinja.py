734c6f206e3793a95cffefbe77d80e5885d46b03
JDSecDef <james.duncan@outlook.com>
2020-09-28 06:05:53 -0700

    Added file system check and fixed the new and updated controls for the current month.

--- ISMNinja.py
+++ ISMNinja.py
@@ -3,6 +3,7 @@
 # Ensure new and revised control dates align.
 # Add ISM chapter headings. 
 
+import os.path
 import requests
 import xml.etree.ElementTree as ET
 import re
@@ -10,7 +11,7 @@ import bs4
 import matplotlib.pyplot as plt
 import numpy as np
 from collections import Counter
-import datetime
+from datetime import date
 
 class bcolors:
     HEADER = '\033[95m'
@@ -31,56 +32,99 @@ uniquesecret      = 0
 secretcontrols    = 0
 uniquetopsecret   = 0
 topsecretcontrols = 0
-revisedcontrol    = []
+updatedcontrol    = []
 newcontrol        = []
 controlnumber     = []
 
-# Scrape CGA ISM page to get XML file.  
+# Get date and assign month and year to variables.
+currentdate = date.today()
+currentmonth= currentdate.strftime("%B")
+currentyear = currentdate.strftime("%Y")
+
 url = 'https://www.cyber.gov.au/acsc/view-all-content/ism'
 retrievehtml = requests.get(url, allow_redirects=True)
-
 nosoup = bs4.BeautifulSoup(retrievehtml.text, features="lxml")
-print('\n[+]\tScraping ' + bcolors.OKGREEN + url + bcolors.ENDC + ' to find ISM XML file')
-gethref = (nosoup.find(href=re.compile(".xml")))
-xmlurl = gethref.get('href')
-filename = xmlurl.split("%")
-month = filename[7]
-year = filename[8]
 
-# Retrieve xml file from CGA. 
-print('[+]\tDownloading the ISM XML File from ' + bcolors.OKGREEN + url + bcolors.ENDC)
-retrievexml = requests.get(xmlurl, allow_redirects=True)
-
-# Check if xml file downloaded successfully.
-if '200' in str(retrievexml):
-  print('[+]\tSucessfully downloaded the ' + bcolors.OKGREEN +  month[2:] + ' ' + year[2:] + ' ' + 'ISM.xml ' + bcolors.ENDC)
+print('\n[+]\tChecking if ' + bcolors.OKGREEN + currentmonth + currentyear + 'ISM.xml' + bcolors.ENDC + bcolors.ENDC + ' already exists in current directory.')
+if os.path.isfile('./' + currentmonth + currentyear + 'ISM.xml'):
+  print('[+]\tLocated ' + bcolors.OKGREEN + currentmonth + currentyear + 'ISM.xml' + bcolors.ENDC + ' in current directory.')
+  xmlexists = True
 else:
+    xmlexists = False
+    print('[+]\tISM XML file not found!')
+    # Scrape CGA ISM page to get XML file.  
+    print('[+]\tScraping ' + bcolors.OKGREEN + url + bcolors.ENDC + ' to find ISM XML file.')
+    gethref = (nosoup.find(href=re.compile(".xml")))
+    xmlurl = gethref.get('href')
+
+if xmlexists != True:
+  # Retrieve xml file from CGA. 
+  print('[+]\tDownloading the ISM XML file from ' + bcolors.OKGREEN + url + bcolors.ENDC)
+  retrievexml = requests.get(xmlurl, allow_redirects=True)
+  # Check if xml file downloaded successfully.
+  if '200' in str(retrievexml):
+    print('[+]\tSucessfully downloaded the ' + bcolors.OKGREEN +  currentmonth + ' ' + currentyear + ' ' + 'ISM.xml ' + bcolors.ENDC + '.')
+  else:
     print('ISM XML file failed to download')
     exit()
-open(month[2:] + year[2:] + 'ISM.xml', 'wb').write(retrievexml.content)
+  open(currentmonth + currentyear + 'ISM.xml', 'wb').write(retrievexml.content)
 
-print('[+]\tAnalysing the ' + bcolors.OKGREEN + month[2:] + ' ' + year[2:] + ' ' + 'ISM' + bcolors.ENDC + '\n')
-tree = ET.parse('./' + month[2:] + year[2:] + 'ISM.xml')
+print('[+]\tChecking if ' + bcolors.OKGREEN + currentmonth + currentyear + 'ISM.pdf' + bcolors.ENDC + bcolors.ENDC + ' already exists in current directory.')
+if os.path.isfile('./' + currentmonth + currentyear + 'ISM.pdf'):
+  print('[+]\tLocated ' + bcolors.OKGREEN + currentmonth + currentyear + 'ISM.pdf' + bcolors.ENDC + ' in current directory.')
+  pdfexists = True
+else:
+    pdfexists = False
+    print('[+]\tISM PDF file not found!')
+    # Scrape CGA ISM page to find ISM pdf file.
+    print('[+]\tScraping ' + bcolors.OKGREEN + url + bcolors.ENDC + ' to find ISM PDF file.')
+    getpdf = (nosoup.find(href=re.compile(".pdf")))
+    pdfurl = getpdf.get('href')
+
+if pdfexists != True:
+  retrievepdf = requests.get(pdfurl, allow_redirects=True)
+  if '200' in str(retrievepdf):
+    print('[+]\tSucessfully downloaded the ' + bcolors.OKGREEN +  currentmonth + ' ' + currentyear + ' ' + 'ISM.pdf ' + bcolors.ENDC + '.')
+  else:
+    print('ISM PDF file failed to download')
+    exit()
+  open(currentmonth + currentyear + 'ISM.pdf', 'wb').write(retrievepdf.content)
+
+print('[+]\tAnalysing the ' + bcolors.OKGREEN +  currentmonth + currentyear + 'ISM.xml' + bcolors.ENDC + '.' + '\n')
+tree = ET.parse('./' + currentmonth + currentyear + 'ISM.xml')
 root = tree.getroot()
 
-# Identify new controls and revised controls in the ISM. 
+# Identify new controls and updated controls in the ISM. 
 for control in root.findall('Control'):
-  if int(control.find('Revision').text) ==0:
+  if int(control.find('Revision').text) == 0 and control.find('Updated').text == currentmonth[0:3] + '-' + currentyear[0:2]:
     newcontrol.append(control.find('Updated').text)
-  else:
-    revisedcontrol.append(control.find('Updated').text)
-  if control.find('Updated').text =='Aug-20' and int(control.find('Revision').text) >=1:
-    controlnumber.append(control.find('Identifier').text)
-    
+
+for control in root.findall('Control'):
+  if int(control.find('Revision').text) != 0 and control.find('Updated').text == currentmonth[0:3] + '-' + currentyear[0:2]:
+    updatedcontrol.append(control.find('Updated').text)
+
+# Count new and updated controls in the latest ISM.     
 newcontrolcount = Counter(newcontrol)
-revisedcontrolcount = Counter(revisedcontrol)
+updatedcontrolcount = Counter(updatedcontrol)
 
-# test = {k: v for k, v in sorted(newcontrolcount.items(), key=lambda item: item[0])}
-# print(test)
+# Print number of new controls. 
+if bool(newcontrolcount) == True:
+  for i in newcontrolcount:
+    print("There are " + bcolors.OKRED + str(newcontrolcount[i]) + bcolors.ENDC + " new controls in the " + currentmonth + ' ' + currentyear + " ISM.")
+else: 
+  print("There are " + bcolors.OKRED + '0' + bcolors.ENDC + ' new controls in the ' + currentmonth + ' ' + currentyear + ' ISM.')
 
-for key, value in zip(newcontrolcount.items(), (revisedcontrolcount.items())):
-  print('There are ' + bcolors.OKRED + str(key[1]) + bcolors.ENDC + ' new controls in the ' + bcolors.OKRED + str(key[0]) + ' ISM' + bcolors.ENDC)
-  print('There are ' + bcolors.OKRED + str(value[1]) + bcolors.ENDC + ' updated controls in the ' + bcolors.OKRED + str(value[0]) + ' ISM' + bcolors.ENDC)
+# Print number of updated controls.
+if bool(updatedcontrolcount) == True:
+  for i in updatedcontrolcount:
+    print("There are " + bcolors.OKRED + str(updatedcontrolcount[i]) + bcolors.ENDC + " updated controls in the " + currentmonth + ' ' + currentyear + " ISM.")
+else:
+  print("There are " + bcolors.OKRED + '0' + bcolors.ENDC + ' controls updated in the ' + currentmonth + ' ' + currentyear + ' ISM.')
+
+# This is not working properly.
+#for key, value in zip(newcontrolcount.items(), (revisedcontrolcount.items())):
+ # print('There are ' + bcolors.OKRED + str(key[1]) + bcolors.ENDC + ' new controls in the ' + bcolors.OKRED + str(key[0]) + ' ISM' + bcolors.ENDC)
+  #print('There are ' + bcolors.OKRED + str(value[1]) + bcolors.ENDC + ' updated controls in the ' + bcolors.OKRED + str(value[0]) + ' ISM' + bcolors.ENDC)
 
 for control in root.findall('Control'):
   official = control.find('OFFICIAL').text
@@ -105,12 +149,12 @@ for control in root.findall('Control'):
     topsecretcontrols+=1
 totalcontrols = sum(1 for entry in root.iter('Identifier'))
 
-print('\nThere are ' + bcolors.OKRED + str(uniqueofficial) + bcolors.ENDC + ' unique OFFICIAL controls in the ' + month[2:] + ' '  + year[2:] + ' ISM')
-print('There are ' + bcolors.OKRED + str(officialcontrols) + bcolors.ENDC + ' OFFICIAL Controls in the ' + month[2:] + ' '  + year[2:] + ' ISM')
-print('There are ' + bcolors.OKRED + str(uniqueprotected) + bcolors.ENDC + ' unique PROTECTED controls in the ' + month[2:] + ' '  + year[2:] + ' ISM')
-print('There are ' + bcolors.OKRED + str(protectedcontrols) + bcolors.ENDC + ' PROTECTED Controls in the ' + month[2:] + ' '  + year[2:] + ' ISM')
-print('There are ' + bcolors.OKRED + str(uniquesecret) + bcolors.ENDC + ' unique SECRET controls in the ' + month[2:] + ' '  + year[2:] + ' ISM')
-print('There are ' + bcolors.OKRED + str(secretcontrols) + bcolors.ENDC + ' SECRET Controls in the ' + month[2:] + ' '  + year[2:] + ' ISM')
-print('There are ' + bcolors.OKRED + str(uniquetopsecret) + bcolors.ENDC + ' unique TOP_SECRET controls in the ' + month[2:] + ' '  + year[2:] + ' ISM')
-print('There are ' + bcolors.OKRED + str(topsecretcontrols) + bcolors.ENDC + ' TOP SECRET Controls in the ' + month[2:] + ' '  + year[2:] + ' ISM')
-print('There are ' + bcolors.OKRED + str(totalcontrols) + bcolors.ENDC + ' controls in total in the ' + month[2:] + ' '  + year[2:] + ' ISM')
+print('\nThere are ' + bcolors.OKRED + str(uniqueofficial) + bcolors.ENDC + ' unique OFFICIAL controls in the ' + currentmonth + ' '  + currentyear + ' ISM')
+print('There are ' + bcolors.OKRED + str(officialcontrols) + bcolors.ENDC + ' OFFICIAL Controls in the ' + currentmonth + ' '  + currentyear + ' ISM')
+print('There are ' + bcolors.OKRED + str(uniqueprotected) + bcolors.ENDC + ' unique PROTECTED controls in the ' + currentmonth + ' '  + currentyear + ' ISM')
+print('There are ' + bcolors.OKRED + str(protectedcontrols) + bcolors.ENDC + ' PROTECTED Controls in the ' + currentmonth + ' '  + currentyear + ' ISM')
+print('There are ' + bcolors.OKRED + str(uniquesecret) + bcolors.ENDC + ' unique SECRET controls in the ' + currentmonth + ' '  + currentyear + ' ISM')
+print('There are ' + bcolors.OKRED + str(secretcontrols) + bcolors.ENDC + ' SECRET Controls in the ' + currentmonth + ' '  + currentyear + ' ISM')
+print('There are ' + bcolors.OKRED + str(uniquetopsecret) + bcolors.ENDC + ' unique TOP_SECRET controls in the ' + currentmonth + ' '  + currentyear + ' ISM')
+print('There are ' + bcolors.OKRED + str(topsecretcontrols) + bcolors.ENDC + ' TOP SECRET Controls in the ' + currentmonth + ' '  + currentyear + ' ISM')
+print('There are ' + bcolors.OKRED + str(totalcontrols) + bcolors.ENDC + ' controls in total in the ' + currentmonth + ' '  + currentyear + ' ISM')
